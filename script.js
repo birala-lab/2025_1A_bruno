1c450ac02c1c9a1587e7ddb2a6a965e5decae5d0
birala-lab <b.irala@escola.pr.gov.br>
2025-10-22 08:41:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca = document.querySelectorAll(".btn-proximo")
+
+avanca.forEach => {
+    botton.addEventlister('click', function{})
+    const atual =document,queryselect('ativo')
+    const proximopasso = 'passo' + this.getAtribate('data-proximo')
+
+    atual.classlist.remove('ativo')
+    document.getElementsByid(proximopasso).classlist.add('ativo')
+}
